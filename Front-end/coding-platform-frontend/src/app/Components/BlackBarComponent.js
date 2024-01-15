9f927f3015efc0285fc744a6339504e3d2d27ea8
pernelkanic <211001118@rajalakshmi.edu.in>
2024-01-15 13:32:38 +0530

    added topbar

---
+++ Front-end/coding-platform-frontend/src/app/Components/BlackBarComponent.js
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+import LinkComponents from './LinkComponents'
+ 
+export default function BlackBarComponent() {
+  return (
+    
+    <div>
+        <div className='bg-black pt-5 pb-3 pl-3 w-[60rem] flex flex-col gap-8 justify-center'> 
+    <Image
+      src="/erdos.svg"
+      width={250}
+      height={250}
+      alt="Picture of the author"
+
+    />
+    <LinkComponents/>
+    </div>
+    </div>
+  )
+}
