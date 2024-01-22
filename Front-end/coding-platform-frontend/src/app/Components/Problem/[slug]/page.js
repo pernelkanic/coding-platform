0b90fe159d42384199a0840ed2bb2140c93d609c
pernelkanic <whitehattt03@gmail.com>
2024-01-22 18:35:30 +0530

    added problem routes

---
+++ Front-end/coding-platform-frontend/src/app/Components/Problem/[slug]/page.js
@@ -0,0 +1,10 @@
+export default function ProblemsSets  ({params})  {
+    return(
+      <p className="text-2xl">
+        Showing the ValidAnagram for the 
+        slug <strong>{params.slug}</strong>
+      </p>
+    )
+  }
+  
+  
--- Front-end/coding-platform-frontend/src/app/Components/Problems/page.js
+++ Front-end/coding-platform-frontend/src/app/Components/Problems/page.js
@@ -1,9 +1,10 @@
 import BlackBarComponent from "../BlackBarComponent";
 import ProblemsetContainer from "./ProblemSetContainer";
+
 export default function Problems(){
     return(
         <>
-            <div className=" flex flex-col  ml-[30em]">
+            <div className=" flex flex-col justify-center items-center">
            <BlackBarComponent/>
             <p className="mt-10">Problem Set</p>
             <ProblemsetContainer/>
