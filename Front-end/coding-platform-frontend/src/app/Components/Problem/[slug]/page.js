b4f72a1c07cb889bdecb69a1d4ab6cc1a567ac40
pernelkanic <211001118@rajalakshmi.edu.in>
2024-01-26 15:52:14 +0530

    added dynamic compilation feature using judge0 api and output is verified

--- Front-end/coding-platform-frontend/src/app/Components/Problem/[slug]/page.js
+++ Front-end/coding-platform-frontend/src/app/Components/Problem/[slug]/page.js
@@ -1,11 +1,14 @@
 'use client'
+import axios from 'axios';
+import { decode as base64_decode, encode as base64_encode } from 'base-64';
 import { useEffect, useState } from "react";
 import CodeEditor from "../../CodeEditor";
-
 export default function ProblemsSets  ({params})  {
   let[problemdata, setproblemdata] = useState([]);
    let[loading, setLoading]= useState(false);
-
+  let[code, setCode] = useState("");
+  let[token, setToken] = useState(null);
+  let[res, setres] = useState("");
     useEffect(()=>{
         setLoading(true);
         fetch("http://localhost:5000/api/problems")
@@ -16,6 +19,54 @@ export default function ProblemsSets  ({params})  {
             
         })
     },[])
+   async function handleClick(){
+      
+      const options = {
+        method: 'POST',
+        url: 'https://judge0-ce.p.rapidapi.com/submissions',
+        params: {
+          base64_encoded: 'true',
+          fields: '*'
+        },
+        headers: {
+          'content-type': 'application/json',
+          'Content-Type': 'application/json',
+          'X-RapidAPI-Key': 'c57044529amsh40f0210b6269090p1e8d62jsn94da47e1fa32',
+          'X-RapidAPI-Host': 'judge0-ce.p.rapidapi.com'
+        },
+        data: {
+          language_id: 62,
+          source_code: base64_encode(code),
+        }
+      };
+      
+      try {
+        const response = await axios.request(options);
+        await setToken(response.data);
+       
+        getResult(token)
+      } catch (error) {
+        console.error(error);
+      }
+    }
+   async function getResult({token}){
+    const geturl = `https://judge0-ce.p.rapidapi.com/submissions/${token}?base64_encoded=true&fields=*`;
+          const getoptions = {
+            method: 'GET',
+            headers: {
+              'X-RapidAPI-Key': 'c57044529amsh40f0210b6269090p1e8d62jsn94da47e1fa32',
+              'X-RapidAPI-Host': 'judge0-ce.p.rapidapi.com'
+            }
+          };
+
+          try {
+            const response = await fetch(geturl, getoptions);
+            const result = await response.text();
+            setres(JSON.parse(result).stdout);
+          } catch (error) {
+            console.error(error);
+          }
+        }
   return(
     <div className=" flex justify-center items-center ">
         
@@ -41,7 +92,14 @@ export default function ProblemsSets  ({params})  {
           })
         }
         <div className="w-full mt-10">
-        <CodeEditor/>
+        <CodeEditor setCode = {setCode} />
+        <div className="flex justify-end mt-6 gap-6 mr-8">
+          <button className=" bg-[#454545] text-white p-2 rounded-md" onClick={handleClick}>Run</button>
+          <button className="bg-[#2CBB5D] text-white p-2 rounded-md">Submit</button>
+        </div>
+          {
+           base64_decode(res)
+          }
         </div>
         </div>
       </div>
