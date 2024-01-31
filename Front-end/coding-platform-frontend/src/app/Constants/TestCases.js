e6558f6e71103f0c999b68d0ef88697e125f9764
pernelkanic <211001118@rajalakshmi.edu.in>
2024-01-31 22:26:29 +0530

    added test cases for each problem and compared it with stdout, need to test bufferreader

---
+++ Front-end/coding-platform-frontend/src/app/Constants/TestCases.js
@@ -0,0 +1,15 @@
+export const TestCases =[
+    {
+        title:"Valid-Anagram",
+        testcase : "2",
+        inputs:  "anagram nagaram rat car",
+        outputs:"true false"
+    },
+    {   
+        title:"two-sum",
+        testcase : "2",
+        inputs:  "4 2 7 11 15 9 3 3 2 4 6",
+        outputs:"1 2"
+    }
+
+]
